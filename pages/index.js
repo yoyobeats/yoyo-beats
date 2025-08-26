e9f8de394fe3ef2213fd0ae963b15ad39902519b
yoyobeats <yoyolamar123@gmail.com>
2025-08-26 13:17:30 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Yoyo Beats!</h1>
+      <p>Your music, your vibes.</p>
+    </div>
+  )
+}
